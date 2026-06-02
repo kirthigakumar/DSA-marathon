c9b68b2147355303b4b175d3f4b240063c0c804f
kirthigakumar <kirthigakumar30@gmail.com>
2026-06-02 10:52:40 +0530

    Add files via upload

---
+++ main - Copy.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class main{
+    public static void main(String[] args){
+        System.out.println("input");
+        Scanner scan=new Scanner(System.in);
+        int a= scan.nextInt();
+        String b=scan.next();
+        char c=scan.next().charAt(0);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
