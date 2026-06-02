3f962fda4812351086f597ae1e6f878fa6a3e8cb
kirthigakumar <kirthigakumar30@gmail.com>
2026-06-02 10:34:38 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 class main{
     public static void main(String[] args){
-        System.out.println(" just Enter what you want");
+        System.out.println("input");
         Scanner scan=new Scanner(System.in);
-        String num=scan.next();
-        String secondnum=scan.next();
-        System.out.println(num);
-        System.out.println(secondnum); 
+        int a= scan.nextInt();
+        String b=scan.next();
+        char c=scan.next().charAt(0);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
