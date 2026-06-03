4cbfe4dfb3d847088f32f4f509ea0edc72e97f43
kirthigakumar <kirthigakumar30@gmail.com>
2026-06-03 11:58:12 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class loop{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        for(int i=0;i<b;i++){
+            System.out.println(a);
+        }
+    }
+}
