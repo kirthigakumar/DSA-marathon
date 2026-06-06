a6e5932f12fe3a1892753d470ab6bf25450cd92e
kirthiga.k <kirthigakumar30@gmail.com>
2026-06-06 20:52:55 +0530

    Add files via upload

---
+++ pattern13.java
@@ -0,0 +1,16 @@
+class pattern13{
+    public static void main(String[] args){
+        int n=3;
+        int count=1;
+        for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(count);
+
+            System.out.print(" ");
+            count++;
+
+        }
+        System.out.println();
+        }
+    }
+}
