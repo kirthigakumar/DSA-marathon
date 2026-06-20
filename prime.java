38c81ac03ef67cb396a2a7031e3741a61ee14fc0
kirthiga.k <kirthigakumar30@gmail.com>
2026-06-20 11:45:45 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,17 @@
+class prime{
+    public static void main(String[] args){
+  
+          int n=11;
+          for(int x=2;x<=n;x++){
+            int flag=0;
+            for(int i=2;i<x;i++){
+                if(x%i==0){
+                    flag=1;
+                }
+            }
+            if(flag==0){
+                System.out.println(x);
+            }
+          }
+        }
+    }
