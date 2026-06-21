278858f7b9ba136226c79052f0a7a1617804bcab
kirthiga.k <kirthigakumar30@gmail.com>
2026-06-21 15:33:51 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,12 @@
+class swap{
+    public static void main(String[] args){
+        int a=7;
+        int b=4;
+        int temp;
+        System.out.println("before swapping:" +a+" "+b);
+        temp=a;
+        a=b;
+        b=temp;
+         System.out.println("after swap:"+ a+" "+b);
+    }
+}
