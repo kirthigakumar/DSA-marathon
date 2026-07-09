307a34d79068d7004fcf40088498ae26dd716a2b
kirthiga.k <kirthigakumar30@gmail.com>
2026-07-09 17:42:54 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,20 @@
+class string{
+    public static void main(String[] args){
+        String s="geeksforgeeks";
+        int maxcount=0;
+        char ans='a';
+        for(int i=0;i<s.length();i++){
+            int count =0;
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    count++;
+                }
+            }
+            if(count>maxcount||count==maxcount&&s.charAt(i)<ans){
+                maxcount =count;
+                ans=s.charAt(i);
+            }
+        }
+        System.out.println(ans);
+    }
+}
